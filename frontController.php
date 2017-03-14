e050d6eeaa7780f084879fff31a5390be5870192
Harshada gothankar <hgharshadagothankar@gmail.com>
2017-03-14 17:34:13 -0400

    new updates
    new updates

---
+++ frontController.php
@@ -0,0 +1,65 @@
+<html>
+<head>
+<title>
+	ALL REAL FOOD
+</title>
+    <link rel="shortcut icon" href="main/images/pos.jpg">
+
+  <link href="main/css/bootstrap.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="main/css/DT_bootstrap.css">
+  
+  <link rel="stylesheet" href="main/css/font-awesome.min.css">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+    </style>
+    <link href="main/css/bootstrap-responsive.css" rel="stylesheet">
+
+<link href="style.css" media="screen" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="container-fluid">
+      <div class="row-fluid">
+		<div class="span4">
+		</div>
+	
+</div>
+<div id="login1">
+
+
+
+			<font style=" font:bold 44px 'Aleo'; text-shadow:1px 1px 15px #000; color:#fff;"><center> About MyMart</center></font>
+		<br>
+	<font style=" font:bold 18px 'Aleo'; text-shadow:1px 1px 1px red; color:black;">This is a Point of sales system for our own brand “ All Real Food”. There are two users of the system
+		<br>
+		1.	Admin<br>
+	2.	Cashier
+<br>
+	Both users have different privilages according to their roles. As per name, Admin is superuser he can add items, delete items, add/delete suppliers and customers.
+	Cashier only can view items in the inventory and can generate the invoice after the products get sold.
+</font>
+		
+<!--<div class="input-prepend">
+		<span style="height:30px; width:25px;" class="add-on"><i class="icon-user icon-2x"></i></span><input style="height:40px;" type="text" name="username" Placeholder="Username" required/><br>
+</div>
+<div class="input-prepend">
+	<span style="height:30px; width:25px;" class="add-on"><i class="icon-lock icon-2x"></i></span><input type="password" style="height:40px;" name="password" Placeholder="Password" required/><br>
+		</div>-->
+		<div class="qwe">
+			<form action="index.php" method="get">
+		 <button class="btn btn-large btn-primary btn-block pull-right" href="dashboard.html" type="submit"><i class="icon-signin icon-large"></i> Login</button>
+			</form>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
